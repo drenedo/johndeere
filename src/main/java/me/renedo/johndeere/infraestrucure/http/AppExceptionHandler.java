49db92e3f4c3e938692eeb702d170cda4967b4ca
Diego Renedo <diego.renedo@orquest.com>
2025-01-21 21:51:07 +0100

    Exception Handler

---
+++ src/main/java/me/renedo/johndeere/infraestrucure/http/AppExceptionHandler.java
@@ -0,0 +1,18 @@
+package me.renedo.johndeere.infraestrucure.http;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorResponse notFoundException(IllegalArgumentException ex) {
+        return new ErrorResponse(ex.getMessage());
+    }
+}
